3a94f826ea3eb1b21d1a982b979746b4807fc3e5
mulempyo <luckyp1@naver.com>
2024-12-05 21:27:45 +0900

    success costmap_update

--- src/costmap_update.cpp
+++ src/costmap_update.cpp
@@ -4,45 +4,53 @@
 #include <sensor_msgs/LaserScan.h>
 #include <geometry_msgs/Point.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include "tf2_ros/buffer.h"
+#include "tf2_ros/transform_listener.h"
 
 namespace update{
 
 void MapUpdate::laserReceived(const sensor_msgs::LaserScanConstPtr& laser_scan){
-   ROS_WARN("laser scan data received function start");
-   laser_scan_ = laser_scan;
+   global_costmap_ = global_costmap_ros_->getCostmap();
+   updateCostmap(laser_scan, global_costmap_);
 }
 
-void MapUpdate::updateCostmap(costmap_2d::Costmap2D* costmap){  //using global planner
+void MapUpdate::updateCostmap(const sensor_msgs::LaserScanConstPtr& laser_scan_, costmap_2d::Costmap2D* costmap){  //using global planner
 
     if (!laser_scan_) {
       ROS_WARN("laser_scan_ is null");
     }
 
-    std::vector<geometry_msgs::Point> obstacles;
-    const sensor_msgs::LaserScan& scan = *laser_scan_;
-    double angle = scan.angle_min;
+    if (!costmap) {
+      ROS_WARN("costmap is null");
+    }
 
-    for (const auto& range : scan.ranges) {
-        if (range >= scan.range_min && range <= scan.range_max) {
+    ROS_INFO("updateCostmap start");
+
+    std::vector<geometry_msgs::Point> obstacles;
+    double angle = laser_scan_->angle_min;
+    ROS_INFO("updateCostmap for (const auto& range : laser_scan_->ranges) start");
+    for (const auto& range : laser_scan_->ranges) {
+        if (range >= laser_scan_->range_min && range <= laser_scan_->range_max) {
             geometry_msgs::Point obstacle;
             obstacle.x = range * std::cos(angle);
             obstacle.y = range * std::sin(angle);
             obstacle.z = 0.0;
             obstacles.push_back(obstacle);
         }
-        angle += scan.angle_increment;
+        angle += laser_scan_->angle_increment;
     }
 
     unsigned int size_x = costmap->getSizeInCellsX();
     unsigned int size_y = costmap->getSizeInCellsY();
-
+    ROS_WARN("updateCostmap for (const auto& obs : obstacles) start");
     for (const auto& obs : obstacles) {
         unsigned int mx, my;
         if (costmap->worldToMap(obs.x, obs.y, mx, my)) {
+            ROS_INFO("make Costmap start.");
             costmap->setCost(mx, my, costmap_2d::LETHAL_OBSTACLE);
         }
     }
-
+    ROS_WARN("updateCostmap for (unsigned int i = 0; i < size_x; ++i) start");
     for (unsigned int i = 0; i < size_x; ++i) {
         for (unsigned int j = 0; j < size_y; ++j) {
             if (costmap->getCost(i, j) == costmap_2d::LETHAL_OBSTACLE) {
@@ -55,6 +63,7 @@ void MapUpdate::updateCostmap(costmap_2d::Costmap2D* costmap){  //using global p
                     }
                 }
                 if (!still_obstacle) {
+                    ROS_INFO("remove Costmap start");
                     costmap->setCost(i, j, costmap_2d::FREE_SPACE);
                 }
             }
@@ -68,10 +77,16 @@ void MapUpdate::updateCostmap(costmap_2d::Costmap2D* costmap){  //using global p
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "update_map");
+
     ros::NodeHandle nh;
-    update::MapUpdate updator;
-    
-    updator.sub = nh.subscribe("scan", 5, &update::MapUpdate::laserReceived, &updator);
+    update::MapUpdate update_;
+   
+    tf2_ros::Buffer buffer(ros::Duration(10));
+    tf2_ros::TransformListener tf(buffer);
+    update_.global_costmap_ros_ = new costmap_2d::Costmap2DROS("update_costmap", buffer);
+ 
+    update_.sub = nh.subscribe("scan",10,&update::MapUpdate::laserReceived, &update_);
+    update_.pub = nh.advertise<sensor_msgs::LaserScan>("laser_scan",10);
 
     ros::spin();
     return 0;
