0d0093e1b10ba47b151379360708e90aa975b58f
mulempyo <luckyp1@naver.com>
2024-12-08 07:10:59 +0000

    update code tf error

--- src/costmap_update.cpp
+++ src/costmap_update.cpp
@@ -13,9 +13,14 @@
 namespace update{
 
 MapUpdate::MapUpdate(){
-  tf_.reset(new tf2_ros::Buffer);
-  tf_->setUsingDedicatedThread(true);
-  tfl_.reset(new tf2_ros::TransformListener(*tf_));
+  buffer_ = std::make_shared<tf2_ros::Buffer>(ros::Duration(10));
+  tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*buffer_);
+  update_costmap_ros_ = new costmap_2d::Costmap2DROS("update_costmap", *buffer_);
+  update_costmap_ros_->start(); 
+}
+
+MapUpdate::~MapUpdate(){
+   delete update_costmap_ros_;
 }
 
 void MapUpdate::laserReceived(const sensor_msgs::LaserScanConstPtr& laser_scan){
@@ -27,10 +32,12 @@ void MapUpdate::updateCostmap(const sensor_msgs::LaserScanConstPtr& laser_scan_,
 
     if (!laser_scan_) {
       ROS_WARN("laser_scan_ is null");
+      return;
     }
 
     if (!costmap) {
       ROS_WARN("costmap is null");
+      return;
     }
 
     std::vector<geometry_msgs::Point> obstacles;
@@ -75,44 +82,70 @@ void MapUpdate::updateCostmap(const sensor_msgs::LaserScanConstPtr& laser_scan_,
         }
     }
    
-   nav_msgs::OccupancyGrid grid;
-   grid.header.stamp = ros::Time::now();
-   grid.header.frame_id = "odom";
-   grid.info.resolution = costmap->getResolution();
-   grid.info.width = costmap->getSizeInCellsX();
-   grid.info.height = costmap->getSizeInCellsY();
-   grid.info.origin.position.x = costmap->getOriginX();
-   grid.info.origin.position.y = costmap->getOriginY();
+  nav_msgs::OccupancyGrid grid;
+  grid.header.stamp = ros::Time::now();
+  grid.header.frame_id = "odom";
+  grid.info.resolution = costmap->getResolution();
+  grid.info.width = costmap->getSizeInCellsX();
+  grid.info.height = costmap->getSizeInCellsY();
+  grid.info.origin.position.x = costmap->getOriginX();
+  grid.info.origin.position.y = costmap->getOriginY();
+  grid.data.resize(grid.info.width * grid.info.height);
 
-   nav_msgs::OccupancyGrid local_grid;
-   geometry_msgs::TransformStamped transformStamped = tf_->lookupTransform("base_footprint",    "odom", ros::Time::now(), ros::Duration(1.0));
-   tf2::doTransform(grid, local_grid, transformStamped);
-   pub.publish(local_grid); 
-  } 
+  for (unsigned int i = 0; i < grid.info.width; ++i) {
+    for (unsigned int j = 0; j < grid.info.height; ++j) {
+        unsigned int index = j * grid.info.width + i;
+        grid.data[index] = costmap->getCost(i, j);
+      }
+  }
 
 
-}//namespace
+  if (!buffer_->canTransform("base_footprint", "odom", ros::Time::now(), ros::Duration(1.0))) {
+    ROS_WARN("Cannot transform from base_footprint to odom");
+    return;
+  }
+
+  geometry_msgs::TransformStamped transformStamped;
+  try {
+      transformStamped = buffer_->lookupTransform("base_footprint", "odom", ros::Time::now(), ros::Duration(1.0));
+  } catch (tf2::TransformException& ex) {
+      ROS_WARN("Transform error: %s", ex.what());
+      return;
+  }
+
+  nav_msgs::OccupancyGrid local_grid = grid;
+  local_grid.header.frame_id = "base_footprint";
+
+  geometry_msgs::PointStamped origin_odom, origin_base;
+  origin_odom.point.x = grid.info.origin.position.x;
+  origin_odom.point.y = grid.info.origin.position.y;
+  origin_odom.point.z = 0.0;
+  origin_odom.header.frame_id = "odom";
+
+  tf2::doTransform(origin_odom, origin_base, transformStamped);
+  local_grid.info.origin.position.x = origin_base.point.x;
+  local_grid.info.origin.position.y = origin_base.point.y;
+  pub.publish(local_grid); 
+} 
+
+} // namespace update
 
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "update_map");
 
     ros::NodeHandle nh;
-  
-    update::MapUpdate update_;
    
-    tf2_ros::Buffer buffer(ros::Duration(10));
-    tf2_ros::TransformListener tf(buffer);
-
-    update_.update_costmap_ros_ = new costmap_2d::Costmap2DROS("update_costmap", buffer);
+    update::MapUpdate update_;
   
-    update_.sub = nh.subscribe("scan",10,&update::MapUpdate::laserReceived, &update_);
-    update_.pub = nh.advertise<nav_msgs::OccupancyGrid>("update_costmap",10);
+    update_.sub = nh.subscribe("scan", 10, &update::MapUpdate::laserReceived, &update_);
+    update_.pub = nh.advertise<nav_msgs::OccupancyGrid>("update_costmap", 10);
 
-    //dwa_planner_ros::DWAPlannerROS dwa_;
-    //dwa_.costmap_sub_ = nh.subscribe("update_costmap", 10, &dwa_planner_ros::DWAPlannerROS::costmapCallback, &dwa_);
+    dwa_planner_ros::DWAPlannerROS dwa_;
+    dwa_.costmap_sub_ = nh.subscribe("update_costmap", 10, &dwa_planner_ros::DWAPlannerROS::costmapCallback, &dwa_);
 
     ros::spin();
-    delete update_.update_costmap_ros_;
+
     return 0;
 }
+
