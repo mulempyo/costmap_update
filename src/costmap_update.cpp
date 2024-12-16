de93a0ac579f8b20fa2f50641a9c92af39812ea1
mulempyo <luckyp1@naver.com>
2024-12-16 09:35:39 +0000

    add marker zone

--- src/costmap_update.cpp
+++ src/costmap_update.cpp
@@ -1,51 +1,52 @@
 #include <dwa_planner_ros/dwa_planner_ros.h>
 #include <ros/ros.h>
 #include <visualization_msgs/MarkerArray.h>
+#include <array>
+#include <vector>
 
 ros::Publisher marker_array_pub;
+std::array<float, 7> safe1 = {-0.063431f, -0.031137f, 0.0f, 0.0f, 0.0f, 0.19328f, 0.999903f};
+std::array<float, 7> safe2 = {4.273204f, 0.379562f, 0.0f, 0.0f, 0.0f, -0.998399f, 0.056565f};
+std::array<float, 7> safe3 = {0.758307f, -0.584536f, 0.0f, 0.0f, 0.0f, -0.065801f, 0.997833f};
+std::array<float, 7> safe4 = {1.517976f, -0.700481f, 0.0f, 0.0f, 0.0f, 0.077507f, 0.996992f};
+std::array<float, 7> safe5 = {2.307844f, -0.628027f, 0.0f, 0.0f, 0.0f, 0.046726f, 0.998908f};
+std::array<float, 7> safe6 = {3.243371f, -0.544172f, 0.0f, 0.0f, 0.0f, 0.479464f, 0.877562f};
+std::array<float, 7> safe7 = {2.608130f, 0.125702f, 0.0f, 0.0f, 0.0f, -0.999792f, 0.020394f};
+std::array<float, 7> safe8 = {3.987488f, 0.935925f, 0.0f, 0.0f, 0.0f, -0.998293f, 0.058406f};
+std::array<float, 7> safe9 = {2.584035f, 1.041702f, 0.0f, 0.0f, 0.0f, 0.999876f, 0.015761f};
+std::array<float, 7> safe10 = {1.647480f, 1.120834f, 0.0f, 0.0f, 0.0f, 0.999695f, 0.024705f};
+std::array<float, 7> safe11 = {0.597729f, 0.904205f, 0.0f, 0.0f, 0.0f, -0.951543f, 0.307515f};
 
 void markPublish(){
     visualization_msgs::MarkerArray marker_array;
 
-    std::array<float, 7> safe1 = {-0.063431f, -0.031137f, 0.0f, 0.0f, 0.0f, 0.19328f, 0.999903f};
-    std::array<float, 7> safe2 = {4.273204f, 0.379562f, 0.0f, 0.0f, 0.0f, -0.998399f, 0.056565f};
+    std::vector<std::array<float, 7>> safes = {safe3, safe4, safe5, safe6, safe7, safe8, safe9, safe10, safe11};
 
-    for(int i=0; i<5; i++){
+    for(size_t i = 0; i < safes.size(); ++i){
         visualization_msgs::Marker marker;
-        marker.header.frame_id = "map";
+        marker.header.frame_id = "map"; 
         marker.header.stamp = ros::Time::now();
-        marker.ns = "array_namespace";
-        marker.id = i;
-        marker.type = visualization_msgs::Marker::CUBE;
-        marker.action = visualization_msgs::Marker::ADD;
+        marker.ns = "array_namespace"; 
+        marker.id = i; 
+        marker.type = visualization_msgs::Marker::ARROW; 
+        marker.action = visualization_msgs::Marker::ADD; 
 
-        if(i % 2 == 0){
-            marker.pose.position.x = safe1[0];
-            marker.pose.position.y = safe1[1];
-            marker.pose.position.z = safe1[2];
-            marker.pose.orientation.x = safe1[3];
-            marker.pose.orientation.y = safe1[4];
-            marker.pose.orientation.z = safe1[5];
-            marker.pose.orientation.w = safe1[6];
-        }
-        else{
-            marker.pose.position.x = safe2[0];
-            marker.pose.position.y = safe2[1];
-            marker.pose.position.z = safe2[2];
-            marker.pose.orientation.x = safe2[3];
-            marker.pose.orientation.y = safe2[4];
-            marker.pose.orientation.z = safe2[5];
-            marker.pose.orientation.w = safe2[6];
-        }
+        marker.pose.position.x = safes[i][0];
+        marker.pose.position.y = safes[i][1];
+        marker.pose.position.z = safes[i][2];
+        marker.pose.orientation.x = safes[i][3];
+        marker.pose.orientation.y = safes[i][4];
+        marker.pose.orientation.z = safes[i][5];
+        marker.pose.orientation.w = safes[i][6];
 
-        marker.scale.x = 0.1;
-        marker.scale.y = 0.1;
-        marker.scale.z = 0.1;
+        marker.scale.x = 0.1; 
+        marker.scale.y = 0.03; 
+        marker.scale.z = 0.03; 
 
-        marker.color.r = 0.0f;
-        marker.color.g = 0.0f;
-        marker.color.b = 1.0f;
-        marker.color.a = 1.0f;
+        marker.color.r = 0.0f; 
+        marker.color.g = 0.0f; 
+        marker.color.b = 1.0f; 
+        marker.color.a = 1.0f; 
 
         marker.lifetime = ros::Duration();
 
@@ -54,7 +55,6 @@ void markPublish(){
 
     marker_array_pub.publish(marker_array);
 }
-
 
 int main(int argc, char** argv)
 {
