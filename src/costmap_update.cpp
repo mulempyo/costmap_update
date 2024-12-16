81c71ced9d8a91e2a6c81274ca7c5447be1631e7
mulempyo <luckyp1@naver.com>
2024-12-16 13:53:22 +0000

    modify marker

--- src/costmap_update.cpp
+++ src/costmap_update.cpp
@@ -5,22 +5,22 @@
 #include <vector>
 
 ros::Publisher marker_array_pub;
-std::array<float, 7> safe1 = {-0.063431f, -0.031137f, 0.0f, 0.0f, 0.0f, 0.19328f, 0.999903f};
-std::array<float, 7> safe2 = {4.273204f, 0.379562f, 0.0f, 0.0f, 0.0f, -0.998399f, 0.056565f};
-std::array<float, 7> safe3 = {0.758307f, -0.584536f, 0.0f, 0.0f, 0.0f, -0.065801f, 0.997833f};
-std::array<float, 7> safe4 = {1.517976f, -0.700481f, 0.0f, 0.0f, 0.0f, 0.077507f, 0.996992f};
-std::array<float, 7> safe5 = {2.307844f, -0.628027f, 0.0f, 0.0f, 0.0f, 0.046726f, 0.998908f};
-std::array<float, 7> safe6 = {3.243371f, -0.544172f, 0.0f, 0.0f, 0.0f, 0.479464f, 0.877562f};
-std::array<float, 7> safe7 = {2.608130f, 0.125702f, 0.0f, 0.0f, 0.0f, -0.999792f, 0.020394f};
-std::array<float, 7> safe8 = {3.987488f, 0.935925f, 0.0f, 0.0f, 0.0f, -0.998293f, 0.058406f};
-std::array<float, 7> safe9 = {2.584035f, 1.041702f, 0.0f, 0.0f, 0.0f, 0.999876f, 0.015761f};
-std::array<float, 7> safe10 = {1.647480f, 1.120834f, 0.0f, 0.0f, 0.0f, 0.999695f, 0.024705f};
-std::array<float, 7> safe11 = {0.597729f, 0.904205f, 0.0f, 0.0f, 0.0f, -0.951543f, 0.307515f};
+std::vector<std::array<float, 7>> safes = {
+        {-0.063431f, -0.031137f, 0.0f, 0.0f, 0.0f, 0.19328f, 0.999903f},
+        {4.273204f, 0.379562f, 0.0f, 0.0f, 0.0f, -0.998399f, 0.056565f},
+        {0.758307f, -0.584536f, 0.0f, 0.0f, 0.0f, -0.065801f, 0.997833f},
+        {1.517976f, -0.700481f, 0.0f, 0.0f, 0.0f, 0.077507f, 0.996992f},
+        {2.307844f, -0.628027f, 0.0f, 0.0f, 0.0f, 0.046726f, 0.998908f},
+        {3.243371f, -0.544172f, 0.0f, 0.0f, 0.0f, 0.479464f, 0.877562f},
+        {2.608130f, 0.125702f, 0.0f, 0.0f, 0.0f, -0.999792f, 0.020394f},
+        {3.987488f, 0.935925f, 0.0f, 0.0f, 0.0f, -0.998293f, 0.058406f},
+        {2.584035f, 1.041702f, 0.0f, 0.0f, 0.0f, 0.999876f, 0.015761f},
+        {1.647480f, 1.120834f, 0.0f, 0.0f, 0.0f, 0.999695f, 0.024705f},
+        {0.597729f, 0.904205f, 0.0f, 0.0f, 0.0f, -0.951543f, 0.307515f}
+        };
 
 void markPublish(){
     visualization_msgs::MarkerArray marker_array;
-
-    std::vector<std::array<float, 7>> safes = {safe3, safe4, safe5, safe6, safe7, safe8, safe9, safe10, safe11};
 
     for(size_t i = 0; i < safes.size(); ++i){
         visualization_msgs::Marker marker;
@@ -38,6 +38,18 @@ void markPublish(){
         marker.pose.orientation.y = safes[i][4];
         marker.pose.orientation.z = safes[i][5];
         marker.pose.orientation.w = safes[i][6];
+
+        tf2::Quaternion q_marker;
+        tf2::fromMsg(marker.pose.orientation, q_marker);
+
+        double additional_yaw = M_PI / 4; 
+        tf2::Quaternion q_additional;
+        q_additional.setRPY(0, 0, additional_yaw);
+
+        tf2::Quaternion q_new = q_marker * q_additional;
+        q_new.normalize();
+
+        marker.pose.orientation = tf2::toMsg(q_new);
 
         marker.scale.x = 0.1; 
         marker.scale.y = 0.03; 
